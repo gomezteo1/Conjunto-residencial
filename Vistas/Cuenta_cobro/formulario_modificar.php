521d0d92f2c4233b5f244a43b8acf6fa0f25ae1c
Mateo Gómez Atehortua <atehortuamateo123@gmail.com>
2020-06-13 22:11:06 -0500

    ya tiene la cuenta de cobro modificar validado

--- Vistas/Cuenta_cobro/formulario_modificar.php
+++ Vistas/Cuenta_cobro/formulario_modificar.php
@@ -90,7 +90,7 @@
 
 									</div>
 									<p class="text-center">
-										<button id="button-Mcc" value='Guardar' name="button-Mcc" class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored bg-primary" type="submit" >
+										<button id="button-Mcc" value='Guardar' name="button-Mcc" value="res-registrar-cuenta_cobro" class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored bg-primary" type="submit" >
 											<i class="zmdi zmdi-plus"></i>
 										</button>
 										<div class="mdl-tooltip" for="btn-addProduct">Modificar CC</div>
@@ -104,6 +104,90 @@
 			</form>
 			<div class="mostrar"></div>
 		</div>
+
+<script>
+
+
+$(document).ready(function(){
+    $('#button-Mcc').click(function(e) {
+    
+        numeroCuenta = $('#numero_cuenta').val()
+        nit = $('#nit').val()
+        slcusuario_inmueble = $('#slcusuario_inmueble').val();
+        var nombreSelect = $("select#slcusuario_inmueble option:selected").attr("nombre");
+        slcmonth = $('#slcmonth').val();
+        var mesSlect = $("select#slcmonth option:selected").attr("fecha");
+        fecha = $('#fecha').val()
+        monto_por_cancelar = $('#monto_por_cancelar').val();
+
+        if (numeroCuenta == "") {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Debes Ingresar El Numero De La Cuenta!',
+            })
+            return false;
+        } else if (numeroCuenta.length <= 5 || numeroCuenta.length >= 13) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'El Numero De Cuenta Debe Tener 6 A 13 Caracteres',
+            })
+            return false;
+        } else if (nit == "") {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Debes Ingresar El Nit!',
+            })
+            return false;
+        } else if (nit.length <= 7 || nit.length >= 25) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'El Nit Debe Tener 8 A 24 Caracteres',
+            })
+            return false;
+        } else if (slcusuario_inmueble == undefined || slcusuario_inmueble == "") {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Debes Ingresar Los Datos De La Cuenta!',
+            })
+            return false;
+        } else if (slcmonth == undefined || slcmonth == "") {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Debes Ingresar El Mes Y Tarifa De La Cuenta!',
+            })
+            return false;
+        } else if (monto_por_cancelar == "") {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Debes Ingresar El Monto!',
+            })
+            return false;
+        } else if (monto_por_cancelar.length <= 0 || monto_por_cancelar.length >= 9) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'El Monto Debe Tener 1 A 8 Caracteres',
+            })
+            return false;
+        }else {
+            Swal.fire({
+				title: "Hecho!",
+				text: "Se Ha Registrado Correctamente",
+				icon: "success",
+				button: "Continuar",
+            });
+        }
+
+    });
+});
+</script>
 	</body>
 </html>
 
